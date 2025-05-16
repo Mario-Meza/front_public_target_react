063317db8f4ec4d0ee57fc181467b49fe1247eef
mario.meza <meza.meza.alt@gmail.com>
2025-05-16 01:27:08 -0600

    dist regenerado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
-content: [
-  "./index.html",
-  "./src/**/*.{js,jsx}"
-]
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",   // todos tus componentes
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
